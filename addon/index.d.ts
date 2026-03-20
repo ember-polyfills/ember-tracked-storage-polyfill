5e316b2303cbeecdd67b32a26c0ff6aa2e556258
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-03-20 14:43:41 -0400

    Convert to v2
    fix"

--- addon/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare const STORAGE: unique symbol;
-export interface TrackedStorage<T> {
-    [STORAGE]: T;
-}
-export declare function createStorage<T = unknown>(initialValue: T, isEqual?: (a: T, b: T) => boolean): TrackedStorage<T>;
-export declare function getValue<T>(storage: TrackedStorage<T>): T;
-declare type TrackedStorageValue<T extends TrackedStorage<unknown>> = T extends TrackedStorage<infer U> ? U : never;
-export declare function setValue<T extends TrackedStorage<unknown>>(storage: T, value: TrackedStorageValue<T>): void;
-export {};
